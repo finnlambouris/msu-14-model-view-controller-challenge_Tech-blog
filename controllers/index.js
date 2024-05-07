3639468423e0f2c5e76d80b5b60ea974ae4144c0
finntendoverse <finnlambouris616@gmail.com>
2024-05-06 23:33:47 -0400

    added session storage

--- controllers/index.js
+++ controllers/index.js
@@ -10,33 +10,26 @@ router.get("/login", (req, res) => {
   res.render("login");
 });
 
-router.get("/api/users/login", async (req, res) => {
-  const userData = await User.findAll();
-  res.json(userData);
-});
-
 router.post('/api/users/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
     if (!userData) {
-      return res.status(400).send("Login failed. Please try again.");
+      return res.status(400).json(userData);
     }
 
     const validPassword = await bcrypt.compare(req.body.password, userData.password);
     if (!validPassword) {
-      return res.status(400).send("Login failed. Please try again.");
+      return res.status(400).json(userData);
     }
 
-    return res.json(userData);
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+    });
 
-    // req.session.save(() => {
-    //   req.session.user_id = userData.id;
-    //   req.session.logged_in = true;
-      
-    //   res.json({ user: userData, message: 'You are now logged in!' });
-    // });
-  } catch {
-    return res.status(400).send("Login failed. Please try again.");
+    return res.status(200).json(userData);
+  } catch (err) {
+    return res.status(400).json(err);
   }
 });
 
@@ -46,7 +39,13 @@ router.post("/api/users", async (req, res) => {
       email: req.body.email,
       password: req.body.password,
     });
-    res.status(200).json(userData);
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+    });
+
+    return res.status(200).json(userData);
   } catch (err) {
     res.status(400).json(err);
   }
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const { config } = require('dotenv');
+require('dotenv').config();
 const path = require('path');
+const sequelize = require('./config/connection.js');
 
 const express = require('express');
 const app = express();
@@ -14,10 +15,26 @@ const handlebars = expressHandlebars.create({});
 app.engine("handlebars", handlebars.engine);
 app.set("view engine", "handlebars");
 
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+app.use(session({
+secret: process.env.SECRET,
+cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+},
+resave: false,
+saveUninitialized: true,
+store: new SequelizeStore({
+    db: sequelize
+})
+}));
+
 const routes = require('./controllers/index.js');
 app.use(routes);
 
-const sequelize = require('./config/connection.js');
 sequelize.sync().then(() => {
     app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}!`));
 });
